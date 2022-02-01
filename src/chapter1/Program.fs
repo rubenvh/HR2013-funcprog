d589d43362976cac17cc3e3fb2393f50ebc48e75
Ruben Van Havermaet <ruben.van.havermaet@gmail.com>
2022-02-01 20:33:34 +0100

    solution for ex 1.5

--- src/chapter1/Program.fs
+++ src/chapter1/Program.fs
@@ -14,9 +14,16 @@ let rec f n =
     | 0 -> 0
     | x when x > 0 -> x + f (n - 1)
     | _ -> failwith "argument should be positive number"
+
+// 1.5
+let rec fib n =
+    match n with
+    | 0 -> 0
+    | 1 -> 1
+    | x -> fib(x-1) + fib(x-2)
     
 [<EntryPoint>]
 let main argv =
-    let result = f 4 
+    let result = fib 7
     printfn "%A" result 
     0
