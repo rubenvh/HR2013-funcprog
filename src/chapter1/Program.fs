3d99e3f0ec93896622a45a9028804c0a264a85f6
Ruben Van Havermaet <ruben.van.havermaet@gmail.com>
2022-02-01 13:52:27 +0100

    solve exercise 1.1 -> 1.3

--- src/chapter1/Program.fs
+++ src/chapter1/Program.fs
@@ -1,7 +1,15 @@
+// 1.1
 let g n = n + 4
+
+// 1.2
 let h (x, y) = System.Math.Sqrt(x * x + y * y)
+
+// 1.3
+let g_exp = fun n -> n + 4
+let h_exp = fun (x,y) -> System.Math.Sqrt(x * x + y * y)
 
 [<EntryPoint>]
 let main argv =
-    printfn "%A" (g 8)
+    let result = h (1.0, 2.0)
+    printfn "%A" result 
     0
