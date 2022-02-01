07975efbe49e872912d8af90a75e14e235381504
Ruben Van Havermaet <ruben.van.havermaet@gmail.com>
2022-02-01 13:59:16 +0100

    solve exercise 1.4

--- src/chapter1/Program.fs
+++ src/chapter1/Program.fs
@@ -8,8 +8,15 @@ let h (x, y) = System.Math.Sqrt(x * x + y * y)
 let g_exp = fun n -> n + 4
 let h_exp = fun (x,y) -> System.Math.Sqrt(x * x + y * y)
 
+// 1.4
+let rec f n =
+    match n with
+    | 0 -> 0
+    | x when x > 0 -> x + f (n - 1)
+    | _ -> failwith "argument should be positive number"
+    
 [<EntryPoint>]
 let main argv =
-    let result = h (1.0, 2.0)
+    let result = f 4 
     printfn "%A" result 
     0
